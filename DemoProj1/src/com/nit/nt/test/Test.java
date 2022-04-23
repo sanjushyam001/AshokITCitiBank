4d36943d3f53f74c8e4bed02a949eb456f3346b1
DEV2 <DEV2@gmail.com>
2022-04-23 10:44:31 -0700

    dev1 modified Test.java

--- DemoProj1/src/com/nit/nt/test/Test.java
+++ DemoProj1/src/com/nit/nt/test/Test.java
@@ -1,5 +1,11 @@
 package com.nit.nt.test;
 
+import com.nit.nt.beans.Utility;
+
 public class Test {
 
+	public static void main(String[] args) {
+		Utility u=new Utility();
+		System.out.println(u.getByDate());
+	}
 }
